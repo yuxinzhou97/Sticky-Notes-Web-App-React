a8f643cf4b2749184047e0dd30fa54f48881ec87
Yuxin Zhou <yuxinzhou1997@gmail.com>
2020-08-03 02:33:05 -0700

    render header and footer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App"
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+
+
+
+
+
+
+
+
+// Requirements:
 //1. Create a new React app.
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
@@ -8,5 +26,3 @@
 //<h1> for a title and a <p> for the content.
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
