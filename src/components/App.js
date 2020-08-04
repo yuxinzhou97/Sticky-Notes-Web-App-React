09fc3a6ffd047bc0d50a4e542b257cafe3c9c86d
Yuxin Zhou <yuxinzhou1997@gmail.com>
2020-08-03 19:01:04 -0700

    create trivia notes and map notes to be shown

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
+import notes from "../notes"
+
+function createNotes(noteItem) {
+  return <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} />
+}
 
 function App() {
-  return (
-    <div>
-    <Header />
-    <Note />
-    <Footer />
-    </div>
-  );
+  return (<div>
+    <Header/>
+    {notes.map(createNotes)}
+    <Footer/>
+  </div>);
 }
 
 export default App;
