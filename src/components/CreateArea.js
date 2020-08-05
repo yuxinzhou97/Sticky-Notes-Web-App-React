13cf6fb2cd1567880b2ae43ee94c648599a447f6
Yuxin Zhou <yuxinzhou1997@gmail.com>
2020-08-05 00:19:47 -0700

    implement add note and delete note functionality

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
 
   function handleChange(event) {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
+
     setNote(prevNote => {
       return {
         ...prevNote,
         [name]: value
       };
-    });  
+    });
   }
 
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
 
   return (
     <div>
       <form>
         <input
-        name="title"
-        onChange={handleChange}
-        value={note.title}
-        placeholder="Title"
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
         />
         <textarea
           name="content"
@@ -33,7 +42,7 @@ function CreateArea() {
           placeholder="Take a note..."
           rows="3"
         />
-        <button>Add</button>
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-
 function Footer() {
-  const currentYear = new Date().getFullYear();
+  const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright© {currentYear}</p>
+      <p>Copyright ⓒ {year}</p>
     </footer>
-  )
+  );
 }
 
 export default Footer;
